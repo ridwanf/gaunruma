9330e1f8de5936428876f50a0718cf4e30b22c5b
ridwan fansuri <ridwan.fansuri@dana.id>
2022-12-25 22:22:27 +0700

    edit footer

--- pages/components/Footer.tsx
+++ pages/components/Footer.tsx
@@ -7,18 +7,8 @@ export default function Footer() {
             <div className="px-3 md:px-0">
               <h3 className="font-bold text-gray-900">About</h3>
               <p className="py-4">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vel mi ut felis tempus commodo nec id erat. Suspendisse consectetur dapibus velit ut lacinia.
+                Gaun Ruma - Bekasi
               </p>
-            </div>
-          </div>
-          <div className="flex w-full lg:w-1/2 lg:justify-end lg:text-right">
-            <div className="px-3 md:px-0">
-              <h3 className="font-bold text-gray-900">Social</h3>
-              <ul className="list-reset items-center pt-3">
-                <li>
-                  <a className="inline-block no-underline hover:text-black hover:underline py-1" href="#">Add social links</a>
-                </li>
-              </ul>
             </div>
           </div>
         </div>
